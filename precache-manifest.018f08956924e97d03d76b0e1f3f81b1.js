8cb585ea3a36010c82b37ca1b61107553f9f1b26
FaustinGarreau <fgarreau@edenschool.fr>
2019-10-24 17:33:26 +0200

    Updates

---
+++ precache-manifest.018f08956924e97d03d76b0e1f3f81b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3435bb37cf94e21dcfa400013a3d7ec5",
+    "url": "/Mastermind/index.html"
+  },
+  {
+    "revision": "3664416ab46434fd811c",
+    "url": "/Mastermind/static/css/main.a155c8d7.chunk.css"
+  },
+  {
+    "revision": "cc220a181b97c47d2fc9",
+    "url": "/Mastermind/static/js/2.d8df32c1.chunk.js"
+  },
+  {
+    "revision": "3664416ab46434fd811c",
+    "url": "/Mastermind/static/js/main.6cec5432.chunk.js"
+  },
+  {
+    "revision": "b461673efdeeb32fb721",
+    "url": "/Mastermind/static/js/runtime-main.1d56527a.js"
+  }
+]);
